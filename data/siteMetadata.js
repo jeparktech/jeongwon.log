a75f777fd15cea0edd0736a976f017fcbefad8a8
jpark978 <jpark978@gatech.edu>
2025-02-18 14:34:28 -0500

    [FIX] email address changed

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'jpark978@gatech.edu',
+  email: 'jepark044@gmail.com',
   github: 'https://github.com/jeparktech',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
